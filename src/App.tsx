afac5aa12a89bd5944148356e14e3af2454e78a8
Frank <faddelia@gmail.com>
2024-08-11 13:33:18 -0500

    Import data from json file and initial print out in cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './sass/main.scss';
 
+import data from './data/data.json';
+
 function App() {
+  const menuItems = data.data;
+
+  console.log(menuItems);
+
   return (
     <main className="main">
       <div className="menu">
         <h2>Desserts</h2>
-        <div className="menu-items">Menu</div>
+        <div className="menu-items">
+          {menuItems.map((item) => (
+            <div className="card">{item.name}</div>
+          ))}
+        </div>
       </div>
       <div className="cart">Cart</div>
     </main>
